b5f0978d8654ba020566a3d2e60ba88a1a31317f
Ivan Malinovsky <swaveful@gmail.com>
2018-06-28 23:59:42 +0300

    fixed redirect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import ProfilePage from "./ProfilePage";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <React.Fragment>
-          <Redirect from="/" to="/EveryInteract" />
+        <Switch>
           <Route exact path="/EveryInteract" component={ProfilePage} />
-        </React.Fragment>
+          <Redirect from="/" to="/EveryInteract" />
+        </Switch>
       </BrowserRouter>
     );
   }
